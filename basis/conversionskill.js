e6ef37004b5495b84037a0ffa3a34c46edae7386
Nitesh Kumar Swarnakar <swarnakarniteshkumar@gmail.com>
2025-07-29 12:14:57 +0000

    conversion of datatype

---
+++ basis/conversionskill.js
@@ -0,0 +1,9 @@
+let score ="nitesh"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumver = Number(score);
+console.log(valueNumver);
+console.log(typeof valueNumver);
+console.log(valueNumver+2);
