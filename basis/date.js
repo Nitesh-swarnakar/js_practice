3f362f65af8318015f29e0a1d8a92d73009bf69d
Nitesh Kumar Swarnakar <swarnakarniteshkumar@gmail.com>
2025-07-30 02:31:47 +0000

    date using in js

---
+++ basis/date.js
@@ -0,0 +1,29 @@
+//date
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+let myCreatedDate = new Date(2024,8,24)
+// console.log(myCreatedDate.toDateString());
+
+let mytimeStamp = Date.now();
+
+// console.log(mytimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now());
+// console.log(Math.round(Date.now()/1000));
+// console.log(Math.round(Date.now()/1000/60));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+
+
