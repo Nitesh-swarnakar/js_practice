bafb5f3de47403349d06f31db11e94bee168cd87
Nitesh Kumar Swarnakar <swarnakarniteshkumar@gmail.com>
2025-07-30 06:39:16 +0000

    array

---
+++ basis/Array.js
@@ -0,0 +1,48 @@
+// Array
+
+
+const myArr =[ 1,2,3,4,5,6,7];
+// myArr.push(8)
+// myArr.push(9)
+// myArr.pop()
+// // myArr.unshift(10)
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(4));
+// console.log(myArr.indexOf(4));
+
+
+
+// const newArr = myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+
+
+//slice, splice
+
+// console.log("A", myArr);
+
+// const myn1= myArr.slice(1,3)
+// console.log(myn1)
+// console.log("B", myArr);
+// console.log("B", myArr);
+
+// const myn2= myArr.splice(1,3)
+// console.log("c", myArr);
+
+// console.log(myn1)
+// console.log(myn2)
+
+
+// const marvelHero =["Iron man", "thor", "Captain America"]
+// const dcHero =['batman','flash', 'superMan'];
+
+// marvelHero.push(dcHero);
+// console.log(marvelHero);
+
+// const  allNewHero = [...marvelHero, ...dcHero]
+// console.log(allNewHero);
+
+
+
